384d16eddeda61952ccb2e66e0113ab6680ac1f5
illusan <jumbo2@op.pl>
2026-05-12 22:12:21 +0200

    settings button

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import customtkinter as ctk
 import pygame # sound
-#from options import OptionsFrame
+from settings import SettingsFrame
 
 app = ctk.CTk()
 app.title("Game inspired by Wordle")
@@ -16,16 +16,23 @@ def set_screen_mode(choice):
         app.attributes("-fullscreen", False)
 
 
+def show_settings():
+    main_menu_frame.pack_forget() # hiding main menu
+    settings_frame.pack(fill="both", expand=True) # showing settings
 
-text_title = ctk.CTkLabel(app, text="Guess the word game!", font=("Arial", 50))
+
+main_menu_frame = ctk.CTkFrame(app, fg_color="transparent")
+main_menu_frame.pack(fill="both", expand=True)
+
+text_title = ctk.CTkLabel(main_menu_frame, text="Guess the word game!", font=("Arial", 50))
 text_title.pack(pady=(100, 0))
 
-text2 = ctk.CTkLabel(app, text="( based on the game Wordle )", font=("Arial", 17), bg_color="transparent")
+text2 = ctk.CTkLabel(main_menu_frame, text="( based on the game Wordle )", font=("Arial", 17), bg_color="transparent")
 text2.pack(pady=(5, 0))
 
 
 
-frame = ctk.CTkFrame(app, bg_color="transparent")
+frame = ctk.CTkFrame(main_menu_frame, bg_color="transparent")
 frame.pack(pady=(100, 0))
 
 button_pl = ctk.CTkButton(frame, text="Graj po Polsku (PL)", width=200, height=50)
@@ -34,16 +41,18 @@ button_pl.grid(row=0, column=0, padx=10, pady=10)
 button_eng = ctk.CTkButton(frame, text="Play in English (ENG)", width=200, height=50)
 button_eng.grid(row=1, column=0, padx=10, pady=10)
 
-button_exit = ctk.CTkButton(frame, text="Exit game", width=200, height=50)
-button_exit.grid(row=2, column=0, padx=10, pady=10)
+button_settings = ctk.CTkButton(frame, text="Settings", width=200, height=50, command=show_settings)
+button_settings.grid(row=2, column=0, padx=10, pady=10)
 
-button_options = ctk.CTkButton(frame, text="Options", width=200, height=50)
-button_options.grid(row=3, column=0, padx=10, pady=10)
+button_exit = ctk.CTkButton(frame, text="Exit game", width=200, height=50, command=app.quit)
+button_exit.grid(row=3, column=0, padx=10, pady=10)
+
+
 
 
 #option = ctk.CTkOptionMenu(frame, values=["Fullscreen", "Windowed"], width=200)
 #option.grid(row=3, column=0, padx=10, pady=10)
 
-
+settings_frame = SettingsFrame(app, switch_screen_callback=set_screen_mode, back_callback=None)
 
 app.mainloop()
---
+++ settings.py
@@ -0,0 +1,9 @@
+import customtkinter as ctk
+
+import customtkinter as ctk
+
+class SettingsFrame(ctk.CTkFrame):
+    def __init__(self, master, switch_screen_callback, back_callback):
+        super().__init__(master)
+        
+        pass
